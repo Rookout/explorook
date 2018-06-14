017e12fa4ad13c3c46a5305ca542ddea4b85ab34
Amit Shani <amit.shani1995@gmail.com>
2018-06-14 19:53:19 +0300

    added auth middleware poc

--- src/api.ts
+++ src/api.ts
@@ -1,25 +1,31 @@
 interface Link {
-    id: string;
-    description: string;
-    url: string;
+  id: string;
+  description: string;
+  url: string;
 }
 
-const links = [{
-    id: "link-0",
-    url: "www.howtographql.com",
-    description: "Fullstack tutorial for GraphQL",
-  }];
+const links: [Link] = [{
+  id: "link-0",
+  url: "www.howtographql.com",
+  description: "Fullstack tutorial for GraphQL",
+}];
 
+let idCount: number = links.length;
 export const resolvers = {
-    Query: {
-      info: () => `This is the API of a Hackernews Clone`,
-      // 2
-      feed: () => links,
-    },
-    // 3
-    Link: {
-      id: (root: Link) => root.id,
-      description: (root: Link) => root.description,
-      url: (root: Link) => root.url,
-    },
-  };
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    // 2
+    feed: () => links,
+  },
+  Mutation: {
+    post: (root: Link, args: any) => {
+      const link: Link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    }
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 // tslint:disable-next-line:no-var-requires
-const { GraphQLServer } = require("graphql-yoga");
+import { GraphQLServer } from "graphql-yoga";
 import { join } from "path";
 import { resolvers } from "./api";
 
-const typeDefs = join(__dirname, `../graphql/schema.graphql`)
+const typeDefs = join(__dirname, `../graphql/schema.graphql`);
 
 // 3
 const server = new GraphQLServer({
@@ -11,7 +11,16 @@ const server = new GraphQLServer({
   typeDefs,
 });
 
+server.express.use((req, res, next) => {
+  const token = req.param("token");
+  if (token === "opensesame") {
+    next();
+  } else {
+    res.send(401);
+  }
+});
+
 export const start = () => {
-    server.start(() => console.log(`Server is running on http://localhost:4000`))
+  server.start(() => console.log(`Server is running on http://localhost:4000`));
 };
 
