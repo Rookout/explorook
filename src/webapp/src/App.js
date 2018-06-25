260044cfb6f3498bdeb3ac3176ff3a080748b72a
Amit Shani <amit.shani1995@gmail.com>
2018-06-25 17:31:27 +0300

    fixed footer responsivness

--- src/webapp/src/App.js
+++ src/webapp/src/App.js
@@ -10,28 +10,24 @@ import { ReposList } from './components/ReposList'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <div id="content-container">
-          <Token />
-          <ReposList />
-        </div>
-        <Footer />
-      </div>
-      // <table className="App" style={{width: "100%"}}>
-      //   <tr>
-      //     <td valign="top" id="content-container">
-      //       <Header />
-      //       <Token />
-      //       <ReposList />
-      //     </td>
-      //   </tr>
-      //   <tr>
-      //     <td valign="bottom">
-      //       <Footer />
-      //     </td>
-      //   </tr>
-      // </table>
+      <table className="App">
+        <tbody>
+          <tr>
+            <td valign="top">
+              <Header />
+              <div id="content-container">
+                <Token />
+                <ReposList />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td valign="bottom">
+              <Footer />
+            </td>
+          </tr>
+        </tbody>
+      </table>
     );
   }
 }
