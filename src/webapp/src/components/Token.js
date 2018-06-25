ced341176594bf20a90cd709d433fd4f9095dc97
Amit Shani <amit.shani1995@gmail.com>
2018-06-25 12:22:29 +0300

    removed lock icon and fixed eye icon

--- src/webapp/src/components/Token.js
+++ src/webapp/src/components/Token.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Button } from '@material-ui/core';
-import { RemoveRedEye, Lock, LockOpen, ContentCopy } from '@material-ui/icons'
+import { VisibilityOff, Visibility, ContentCopy } from '@material-ui/icons'
 import { copyText } from '../utils'
 require = window.require;
 const { ipcRenderer } = require("electron");
@@ -21,24 +21,25 @@ export class Token extends Component {
         copyText(this.state.token);
     }
     onEyeClicked(e) {
-        this.setState({ ...this.state, renderToken: this.state.token });
-        setTimeout(() => {
+        if (this.state.renderToken === hiddenToken) {
+            this.setState({ ...this.state, renderToken: this.state.token });
+        } else {
             this.setState({ ...this.state, renderToken: hiddenToken });
-        }, 3500)
+        }
     }
     render() {
         return (
             <div>
                 <div id="token-title" className="flex">
                     <p className="gray-shaded">Access Token &nbsp;</p>
-                    { this.state.renderToken === hiddenToken ?
-                        <Lock id="token-lock" className="primary" onClick={this.onEyeClicked} />
-                        :
-                        <LockOpen id="token-lock" className="primary" onClick={this.onEyeClicked} />
-                    }
                 </div>
                 <div id="token-wrapper">
-                    <p id="token-box">{this.state.renderToken}<RemoveRedEye onClick={this.onEyeClicked} id="token-show-eye" className="small-icon" /></p>
+                    <p id="token-box">{this.state.renderToken}
+                    { this.state.renderToken === hiddenToken ?
+                    <Visibility onClick={this.onEyeClicked} id="token-show-eye" className="small-icon" />
+                    :
+                    <VisibilityOff onClick={this.onEyeClicked} id="token-show-eye" className="small-icon" />
+                    }</p>
                     <Button onClick={this.onCopyClick} id="copy-token-btn" variant="contained"><ContentCopy style={{ fontSize: 20 }} />&nbsp;Copy</Button>
                 </div>
             </div>
