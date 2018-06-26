3d9ad1ed023f67b23fca08232b39c6f9ce7bbb86
Amit Shani <amit.shani1995@gmail.com>
2018-06-26 10:45:00 +0300

    validating access token on every request

--- src/server.ts
+++ src/server.ts
@@ -2,26 +2,23 @@ import { GraphQLServer } from "graphql-yoga";
 import { join } from "path";
 import { repoMiddleware, resolvers, traversalMiddleware } from "./api";
 
-const typeDefs = join(__dirname, `../graphql/schema.graphql`);
+export const start = (accessToken: string) => {
+  const typeDefs = join(__dirname, `../graphql/schema.graphql`);
 
-// 3
-const server = new GraphQLServer({
-  resolvers,
-  typeDefs,
-  middlewares: [traversalMiddleware, repoMiddleware],
-});
+  const server = new GraphQLServer({
+    resolvers,
+    typeDefs,
+    middlewares: [traversalMiddleware, repoMiddleware],
+  });
 
-server.express.use((req, res, next) => {
-  const token = req.param("token") || req.header("token") || "";
-  if (token === "opensesame") {
-    next();
-  } else {
-    //res.send(401);
-    next();
-  }
-});
-
-export const start = () => {
+  server.express.use((req, res, next) => {
+    const token = req.param("token") || req.header("token") || "";
+    if (token === accessToken) {
+      next();
+    } else {
+      res.send(401);
+    }
+  });
   server.start({ port: 50001 }, (options) => console.log(`Server is running on http://localhost:${options.port}`));
 };
 
