f50e81ac1df5e3d8e2b7c6fa76990d2c885b613c
Amit Shani <amit.shani1995@gmail.com>
2018-06-27 15:47:24 +0300

    deleted unnecessary files

--- index.js
+++
@@ -1,4 +0,0 @@
-// electron-packager automatically looks for index.js at the root folder.
-// our app's entry point is in /dist/index.js. I tried to use "main" in package.json
-// but it breaks other stuff
-require("./dist/index");
