8ceb438ec7af099334e2c306685c6a8e701a5551
Amit Shani <amit.shani1995@gmail.com>
2018-07-01 16:47:12 +0300

    added version bump validation to CI/CD

---
+++ validate_version.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get current version from package.json file
+CURRENT_VERSION="$(node -e 'console.log(require("./package").version)')"
+# Add "v" prefix
+CV_WITH_V="v$CURRENT_VERSION"
+
+# Get all tags exists
+TAGS="$(git tag -l)"
+
+# check if current version already has a tag which means the publish will fail
+[[ $TAGS =~ (^|[[:space:]])$CV_WITH_V($|[[:space:]]) ]] && echo "a release tag with version $CV_WITH_V already exists" && exit 1
+echo 'no version conflicts found'
