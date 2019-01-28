ec2cf9abfc571f9ed419c0150dd1a6c9113e772c
amit <amit.shani1995@gmail.com>
2019-01-28 11:41:40 +0200

    wrong init condition

--- src/exceptionManager.ts
+++ src/exceptionManager.ts
@@ -1,17 +1,17 @@
 import { Client, INotifyOpts, NotifiableError } from "@bugsnag/core";
-import bugsnag from "@bugsnag/js";
+import bugsnag = require("@bugsnag/js");
 import { app } from "electron";
 
 let exceptionManagerInstance: Client;
 
 export const initExceptionManager = () => {
-    if (exceptionManagerInstance) {
+    if (!exceptionManagerInstance) {
         exceptionManagerInstance = bugsnag({
             apiKey: "6e673fda179162f48a2c6b5d159552d2",
             appVersion: app.getVersion(),
             appType: "explorook-electron",
             releaseStage: process.env.development ? "development" : "production"
-        });
+        }, null);
     }
     return exceptionManagerInstance;
 };
