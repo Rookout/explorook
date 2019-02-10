43dd3e2ff76fab33d1c7f2e68d6e289f1ecc1379
amit <amit.shani1995@gmail.com>
2019-02-10 19:30:31 +0200

    prevent bugsnag from exiting when updater throws exception

--- src/exceptionManager.ts
+++ src/exceptionManager.ts
@@ -6,6 +6,10 @@ let exceptionManagerInstance: Client;
 export const initExceptionManager = (releaseStage: string, appVersion: string) => {
     if (!exceptionManagerInstance) {
         exceptionManagerInstance = bugsnag({
+            onUncaughtException: (err: any) => {
+                // override default behaviour to not crash
+                // https://docs.bugsnag.com/platforms/javascript/configuration-options/#onuncaughtexception-node-js-only
+            },
             apiKey: "6e673fda179162f48a2c6b5d159552d2",
             appType: "explorook-electron",
             appVersion,
