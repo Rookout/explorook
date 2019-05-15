d9aae39becf04472b9ab332364a827c27df9c3d0
Amit Shani <amit.shani1995@gmail.com>
2019-05-15 10:17:21 +0300

    allow debugging through hidden button (#61)

--- src/webapp/src/components/Header.js
+++ src/webapp/src/components/Header.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { Close } from '@material-ui/icons';
 import { remote, ipcRenderer } from 'electron';
+import { Menu, MenuItem, ClickAwayListener } from '@material-ui/core';
 
 export class Header extends Component {
     constructor(props) {
         super(props);
         this.closeWindow = this.closeWindow.bind(this);
-        this.state = { version: remote.app.getVersion() }
+        this.state = { 
+            version: remote.app.getVersion(),
+            anchorEl: null,
+            open: false
+        }
     }
 
     closeWindow() {
@@ -18,11 +23,25 @@ export class Header extends Component {
         ipcRenderer.send('hidden');
     }
 
+    onCloseRightClick = e => {
+        e.preventDefault();
+        this.setState({ open: true, anchorEl: e.currentTarget})
+    }
+
+    startDebug = e => {
+        ipcRenderer.send('inspect-all')
+        this.setState({ open: false })
+    }
+
     render() {
         return (
             <div>
-                <div id="close-window-wrapper">
+                <div id="close-window-wrapper" onContextMenu={this.onCloseRightClick}>
                     <Close id="close-window-btn" onClick={this.closeWindow}/>
+                    <Menu anchorEl={this.state.anchorEl} open={this.state.open}>
+                        <MenuItem key="debug" onClick={this.startDebug}>Debug</MenuItem>
+                        <MenuItem key="close" onClick={() => this.setState({ open: false })}>Close</MenuItem>
+                    </Menu>
                 </div>
                 <div className="Header flex">
                     <img src="logo.png" className="Header-logo"/>
