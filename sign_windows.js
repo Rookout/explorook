2f11075de33dcd30753db5ef772c74401d246a01
Mickael Alliel <mickael@rookout.com>
2019-06-19 12:50:26 +0300

    skip dlls

--- sign_windows.js
+++ sign_windows.js
@@ -3,6 +3,11 @@ const package = require("./package.json");
 exports.default = async function(configuration) {
   // do not include passwords or other sensitive data in the file
   // rather create environment variables with sensitive data
+  const PE_PATH = configuration.path;
+  if (PE_PATH.includes(".dll")) {
+    return true;
+  }
+
   const CERTIFICATE_PATH = process.env.WINDOWS_EV_CERTIFICATE_PATH;
   const GOOGLE_HSM_KEY_ID = process.env.GOOGLE_HSM_KEY_ID;
 
@@ -15,7 +20,7 @@ exports.default = async function(configuration) {
     "--name", configuration.name,
     "--url", configuration.site,
     "--replace",
-    `"${configuration.path}"`,
+    `"${PE_PATH}"`,
   ];
 
   require("child_process").execSync(
