14a73246ac83a621d2945beaa860bd96bea55fc2
Mickael Alliel <mickael@rookout.com>
2019-08-01 17:42:20 +0300

    Update React to 16.8.0 + Refactor component to use new state and effect Show EULA only instead of on top of main component

--- src/webapp/src/App.js
+++ src/webapp/src/App.js
@@ -1,48 +1,44 @@
-import React, { Component } from 'react';
-import 'typeface-roboto/index.css'
+import React, { useEffect, useState } from "react";
+import "typeface-roboto/index.css";
 // import 'material-design-icons/iconfont/material-icons.css'
-import './App.css';
-import { Header } from './components/Header'
-import Footer from './components/Footer'
-import { Token } from './components/Token'
-import { ReposList } from './components/ReposList'
-import EulaModal from './components/EulaModal'
-import { ipcRenderer } from 'electron';
-import './App.scss';
+import "./App.css";
+import { Header } from "./components/Header";
+import Footer from "./components/Footer";
+import { Token } from "./components/Token";
+import { ReposList } from "./components/ReposList";
+import { EulaModal } from "./components/EulaModal";
+import { ipcRenderer } from "electron";
+import "./App.scss";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true
-    }
+export const App = ({ ...props }) => {
+  const [loading, setLoading] = useState(true);
+  const [hasSignedEula, setHasSignedEula] = useState(ipcRenderer.sendSync("has-signed-eula"));
+
+  useEffect(() => {
     ipcRenderer.on("indexer-worker-id", (e, id) => {
       window.indexWorkerId = id;
-      this.setState({ loading: false })
+      setLoading(false);
     });
     ipcRenderer.send("app-window-up");
-  }
+  }, []);
 
-  render() {
-    if (this.state.loading) return (<div />);
-    return (
-      <div style={{ display: 'flex', height: '100%', flexDirection: 'column' }}>
-        <div>
-          <Header />
-          <Token />
-        </div>
-        <div style={{ overflow: 'auto'}}>
-          <div id="content-container">
-            <ReposList />
-            <EulaModal />
-          </div>
-        </div>
-        <div style={{ justifyContent: 'flex-end', marginTop: 'auto' }}>
-          <Footer />
+  if (loading) { return (<div />); }
+
+  return (
+    <div style={{ display: "flex", height: "100%", flexDirection: "column" }}>
+      <div>
+        <Header />
+        <Token />
+      </div>
+      <div style={{ overflow: "auto"}}>
+        <div id="content-container">
+          { hasSignedEula && <ReposList /> }
+          { !hasSignedEula && <EulaModal setSignedEula={setHasSignedEula} {...props} /> }
         </div>
       </div>
-    );
-  }
-}
-
-export default App;
+      <div style={{ justifyContent: "flex-end", marginTop: "auto" }}>
+        <Footer />
+      </div>
+    </div>
+  );
+};
