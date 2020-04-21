ff9457bcc30bd14291e017d3a79564acd9b93d07
Amit Shani <amit@rookout.com>
2020-04-21 16:26:56 +0300

    fix gs links (#145)

--- upload_release_to_gs.sh
+++ upload_release_to_gs.sh
@@ -9,8 +9,11 @@ INSTALLERS_DIR="/Users/distiller/project/installers/"
 WINDOWS_FILE_NAME="Rookout Desktop App Setup ${VERSION}.exe"
 WINDOWS_NO_SPACE_FILE_NAME="Rookout-Desktop-App-Setup-${VERSION}.exe"
 MAC_DMG_FILE_NAME="Rookout Desktop App-${VERSION}.dmg"
+MAC_DMG_NO_SPACE_FILE_NAME="Rookout-Desktop-App-${VERSION}.dmg"
 MAC_ZIP_FILE_NAME="Rookout Desktop App-${VERSION}-mac.zip"
+MAC_ZIP_NO_SPACE_FILE_NAME="Rookout-Desktop-App-${VERSION}-mac.zip"
 LINUX_FILE_NAME="Rookout Desktop App-${VERSION}.AppImage"
+LINUX_NO_SPACE_FILE_NAME="Rookout Desktop App-${VERSION}.AppImage"
 
 
 echo "Starting upload to Google Storage Bucket: ${BUCKET}"
@@ -19,14 +22,14 @@ gsutil cp "${INSTALLERS_DIR}${WINDOWS_FILE_NAME}" "gs://${BUCKET}/explorook/wind
 gsutil acl ch -u AllUsers:R "gs://${BUCKET}/explorook/windows/${WINDOWS_NO_SPACE_FILE_NAME}"
 
 echo "Uploading Mac Installers (DMG+ZIP)..."
-gsutil cp "${INSTALLERS_DIR}${MAC_DMG_FILE_NAME}" "gs://${BUCKET}/explorook/mac/${MAC_DMG_FILE_NAME}"
-gsutil acl ch -u AllUsers:R "gs://${BUCKET}/explorook/mac/${MAC_DMG_FILE_NAME}"
+gsutil cp "${INSTALLERS_DIR}${MAC_DMG_FILE_NAME}" "gs://${BUCKET}/explorook/mac/${MAC_DMG_NO_SPACE_FILE_NAME}"
+gsutil acl ch -u AllUsers:R "gs://${BUCKET}/explorook/mac/${MAC_DMG_NO_SPACE_FILE_NAME}"
 
-gsutil cp "${INSTALLERS_DIR}${MAC_ZIP_FILE_NAME}" "gs://${BUCKET}/explorook/mac/${MAC_ZIP_FILE_NAME}"
-gsutil acl ch -u AllUsers:R "gs://${BUCKET}/explorook/mac/${MAC_ZIP_FILE_NAME}"
+gsutil cp "${INSTALLERS_DIR}${MAC_ZIP_FILE_NAME}" "gs://${BUCKET}/explorook/mac/${MAC_ZIP_NO_SPACE_FILE_NAME}"
+gsutil acl ch -u AllUsers:R "gs://${BUCKET}/explorook/mac/${MAC_ZIP_NO_SPACE_FILE_NAME}"
 
 echo "Uploading Linux Installer..."
-gsutil cp "${INSTALLERS_DIR}${LINUX_FILE_NAME}" "gs://${BUCKET}/explorook/linux/${LINUX_FILE_NAME}"
-gsutil acl ch -u AllUsers:R "gs://${BUCKET}/explorook/linux/${LINUX_FILE_NAME}"
+gsutil cp "${INSTALLERS_DIR}${LINUX_FILE_NAME}" "gs://${BUCKET}/explorook/linux/${LINUX_NO_SPACE_FILE_NAME}"
+gsutil acl ch -u AllUsers:R "gs://${BUCKET}/explorook/linux/${LINUX_NO_SPACE_FILE_NAME}"
 
 echo "Uploaded release successfully to ${BUCKET} !"
