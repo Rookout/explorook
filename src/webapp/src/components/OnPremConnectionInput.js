01df467693ec6a27de2653d5aaad6ad0c5d647bf
dansela <dan@rookout.com>
2020-05-25 17:25:16 +0300

    Moved really styled components to .style file

--- src/webapp/src/components/OnPremConnectionInput.js
+++ src/webapp/src/components/OnPremConnectionInput.js
@@ -1,14 +1,7 @@
-import {Button, TextField} from "@material-ui/core";
 import React, {useEffect, useState} from "react";
 import {ipcRenderer} from "electron";
 import * as Store from "electron-store";
-import {Check as CheckIcon, Clear as ClearIcon} from "@material-ui/icons";
-
-const connectionStates = {
-    PENDING: 'PENDING',
-    SUCCESS: 'SUCCESS',
-    ERROR: 'ERROR'
-};
+import {connectionStates, OnPremSourceInput, TestConnectionButton} from "./OnPremConnectionInput.style";
 
 export const OnPremTypes = {
     GIT: "git",
@@ -39,37 +32,15 @@ export const OnPremConnectionInput = ({type, label}) => {
         ipcRenderer.sendTo(window.indexWorkerId,`test-${type}-connection`, stringToTest)
     };
 
-    const getTestConnectionButtonContent = () => {
-        switch (connectionState) {
-            case connectionStates.SUCCESS:
-                return <CheckIcon style={{fontSize:'18px'}}/>;
-            case connectionStates.ERROR:
-                return <ClearIcon style={{fontSize:'18px'}}/>;
-            default:
-                return "Test";
-        }
-    }
-
     return <>
-        <TextField
+        <OnPremSourceInput
             label={label}
-            style={{width:"400px", color: "#B6C8D4"}}
             value={connectionString}
-            onChange={e => setConnectionString(e.currentTarget.value)}
-            InputProps={{className: 'on-prem-input'}}
-            InputLabelProps={{style: {color: "#B6C8D4"}}}
+            setValue={e => setConnectionString(e.currentTarget.value)}
         />
-        <Button size="small" style={{
-            padding: 0,
-            marginLeft: "20px",
-            backgroundColor: "#586C7A",
-            color: "white",
-            borderRadius: "30px",
-            boxShadow: "none",
-            width: "111px",
-            height: "25px"
-        }} variant="contained" onClick={() => onTestConnection(connectionString)}>
-            {getTestConnectionButtonContent()}
-        </Button>
+        <TestConnectionButton
+            onTestConnection={() => onTestConnection(connectionString)}
+            connectionState={connectionState}
+        />
     </>
 }
