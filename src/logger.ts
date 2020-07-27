5873f988e952ee301c37a1c0199157a1f435a40b
dansela <dan@rookout.com>
2020-07-27 14:15:02 +0300

    Added git logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,12 +1,23 @@
 import {Logger} from "log4js";
+import {getStoreSafe} from "./explorook-store";
 import {getLibraryFolder} from "./utils";
 
 const path = require("path");
 const log4js = require("log4js");
 
+const store = getStoreSafe();
+let logLevel = store.get("logLevel", null);
+if (!logLevel) {
+    store.set("logLevel", "error");
+    logLevel = "error";
+}
+
 log4js.configure({
-    appenders: { perforce: {type: "file", filename: path.join(getLibraryFolder(), "rookout.log")}},
-    categories: {default: {appenders: ["perforce"], level: "debug"}}
+    appenders: {
+            perforce: {type: "file", filename: path.join(getLibraryFolder(), "rookout.log")},
+            git: {type: "file", filename: path.join(getLibraryFolder(), "rookout.log")}
+        },
+    categories: {default: {appenders: ["perforce", "git"], level: logLevel}}
 });
 
 export const getLogger = (loggerName: string): Logger => log4js.getLogger(loggerName);
