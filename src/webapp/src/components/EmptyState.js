059e9baf8b570de1a7c4d57c7df99f5e0be61b45
Amit Shani <amit.shani1995@gmail.com>
2020-12-20 14:37:45 +0200

    fix img not rendering in packaged version

--- src/webapp/src/components/EmptyState.js
+++ src/webapp/src/components/EmptyState.js
@@ -5,7 +5,7 @@ export const EmptyState = () => {
   return (
     <div style={{ width: "100%", height: "100%", textAlign: 'center', display: 'flex' }}>
       <div style={{ margin: 'auto', display:'flex', flexDirection:'row' }}>
-        <img style={{ width: 120, height: 140, paddingRight: 10 }} src="../bird-on-folder.png"></img>
+        <img style={{ width: 120, height: 140, paddingRight: 10 }} src={process.env.PUBLIC_URL+"/bird-on-folder.png"}></img>
         <div style={{ display: 'flex', flexDirection: 'column' }}>
           <Text style={{ marginBottom: "5px", marginTop: "55px" }}>Sweet! Rookout desktop app is up and</Text>
           <Text style={{ marginTop: 0 }}>and running in the background</Text>
