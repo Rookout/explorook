7c00e400904bb268b14e60051ff599d07fdb1709
dansela <dan@rookout.com>
2021-08-31 17:44:39 +0300

    Making the logger actually work

--- src/logger.ts
+++ src/logger.ts
@@ -18,11 +18,13 @@ if (!logLevel) {
 
 const getLogFileLocation = () => path.join(getLibraryFolder(), "rookout.log");
 
+const appender = require("./logsContainerAppender");
+
 log4js.configure({
     appenders: {
         file: {type: "file", filename: getLogFileLocation()},
         "logs-container": {
-            type: "./logsContainerAppender",
+            type: appender,
             layout: {
                 type: "pattern",
                 pattern: "[%d] [%p] [%m%n]"
