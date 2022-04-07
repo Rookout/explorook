f60bb8ebc421a1c91f4f398be0356ef6986042b2
Urook <uri@rookout.com>
2022-04-07 19:04:40 +0300

    Use require package json instead of import from json

--- src/api.ts
+++ src/api.ts
@@ -35,7 +35,6 @@ import {Repo, repStore} from "./repoStore";
 import {loadingStateUpdateHandler, onAddRepoRequestHandler, onRemoveRepoRequestHandler} from "./server";
 import { getSettings, setSettings } from "./utils";
 const folderDelete = require("folder-delete");
-import packageJson from "../package.json";
 
 // using posix api makes paths consistent across different platforms
 const join = posix.join;
@@ -305,12 +304,12 @@ export const resolvers = {
     },
     appVersion: async (parent: any): Promise<string> => {
       if (process.env.development) {
-        return require("../package.json").version;
+        return require("../package.json")?.version;
       } else if (remote) {
         return remote.app.getVersion();
       } else {
         // remote should exist. but sometimes it's undefined and breaks tests for some reason, so adding a temp fallback
-        return packageJson?.version || "1.8.33";
+        return require("../package.json")?.version || "1.8.34";
       }
     },
     recentLogs: (parent: any): Log[] => {
