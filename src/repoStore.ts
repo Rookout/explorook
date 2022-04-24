acd15070091489f5568ecc6434df5341d5b25d7a
Urook <uri@rookout.com>
2022-04-24 14:35:52 +0300

    Support refreshing all git indices with a single request

--- src/repoStore.ts
+++ src/repoStore.ts
@@ -1,10 +1,10 @@
-import { IStore, getStoreSafe } from './explorook-store';
 import { ipcRenderer } from "electron";
 import fs = require("fs");
-import git = require("isomorphic-git");
-import _ = require("lodash");
-import parseRepo = require("parse-repo");
+import _find from "lodash/find";
+import _forEach from "lodash/forEach";
+import _remove from "lodash/remove";
 import { Repository } from "./common/repository";
+import { getStoreSafe, IStore } from "./explorook-store";
 import { IndexWorker } from "./fsIndexer";
 import { getRepoId } from "./git";
 import {logger} from "./langauge-servers/configStore";
@@ -65,7 +65,7 @@ class RepoStore {
     private repos: Repo[];
 
     constructor() {
-        this.store = getStoreSafe()
+        this.store = getStoreSafe();
         const models = JSON.parse(this.store.get("repositories", "[]")) as Repository[];
         this.allowIndex = JSON.parse(this.store.get("allow-indexing", "true"));
         this.repos = [];
@@ -92,7 +92,7 @@ class RepoStore {
 
     public async add(repo: Repository): Promise<string> {
         try {
-            const existingRepo = _.find(this.repos, r => r.fullpath === repo.fullpath);
+            const existingRepo = _find(this.repos, r => r.fullpath === repo.fullpath);
             if (existingRepo) {
                 return existingRepo.id;
             }
@@ -118,7 +118,7 @@ class RepoStore {
     }
 
     public remove(id: string): boolean {
-        const removed = _.remove(this.repos, (r) => r.id === id);
+        const removed = _remove(this.repos, (r) => r.id === id);
         removed.forEach((r) => {
             r.indexer.deleteIndex();
             ipcRenderer.send("track", "repo-remove", { repoName: r.repoName, repoId: r.id });
@@ -142,7 +142,13 @@ class RepoStore {
     }
 
     public getRepoById(id: string): Repo {
-        return _.find(this.repos,repo => { return id === repo.id})
+        return _find(this.repos, repo => id === repo.id);
+    }
+
+    public reAllIndices() {
+        _forEach(this.repos, (repo: Repo) => {
+            repo.reIndex();
+        });
     }
 }
 
