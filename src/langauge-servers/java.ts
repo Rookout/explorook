deae2586662f67fca4dae9aab90e88b84e6efb93
Urook <uri@rookout.com>
2022-04-26 13:46:21 +0300

    More trials

--- src/langauge-servers/java.ts
+++ src/langauge-servers/java.ts
@@ -1,12 +1,16 @@
 import * as rpc from "@codingame/monaco-jsonrpc";
 import * as path from "path";
+import { WORKSPACE } from "../git";
 import { configLocation, javaLangServerJarLocation, langServerConfigStore } from "./configStore";
 import { JAVA_FILENAME } from "./javaUtils";
 import { launchLangaugeServer } from "./langaugeServer";
 
 export const launchJavaLangaugeServer = (socket: rpc.IWebSocket) => {
     if (langServerConfigStore.doesJavaJarExist() && langServerConfigStore.jdkLocation) {
-        const args = ["-Xmx1G", "-jar", javaLangServerJarLocation, "-configuration", configLocation];
+        const args = ["-Xmx512m",
+            "-Xms512m",
+            "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=,quiet=y",
+            "-jar", javaLangServerJarLocation, "-configuration", configLocation];
 
         const javaBin = path.join(langServerConfigStore.jdkLocation, "bin", JAVA_FILENAME);
 
