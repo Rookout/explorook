1239803ad75cccc0bd6e01fe66703e224610e8d9
Ron Belkin <ron@rookout.com>
2024-06-27 15:50:28 +0300

    Added cache + small refactor

--- src/cors.ts
+++ src/cors.ts
@@ -1,5 +1,6 @@
 import type {CorsOptions, CorsRequest} from "cors";
 import * as cors from "cors";
+import fetch from "node-fetch";
 
 
 const LOCALHOST_ORIGIN = "https://localhost:8080";
@@ -9,18 +10,31 @@ const DYNATRACE_ORIGIN_REGEX = /^https:\/\/.*\.dynatrace(?:labs)?\.com$/;
 const ALLOW_CORS_OPTION: CorsOptions = {origin: true};
 const DENY_CORS_OPTION: CorsOptions = {origin: false};
 
+
+const verifiedOriginsCache = new Set([LOCALHOST_ORIGIN]);
+
+
 const corsOptionsDelegate = async (req: CorsRequest, callback: (err: Error | null, options?: CorsOptions) => void) => {
     try {
         const origin = req.headers.origin;
-        if (origin === LOCALHOST_ORIGIN || ROOKOUT_ORIGIN_REGEX.test(origin)) {
+        if (verifiedOriginsCache.has(origin) || ROOKOUT_ORIGIN_REGEX.test(origin)) {
             callback(null, ALLOW_CORS_OPTION);
             return;
         }
 
         if (!DYNATRACE_ORIGIN_REGEX.test(origin)) {
             callback(null, DENY_CORS_OPTION);
+            return;
         }
 
+        const response = await fetch(`${origin}/platform-reserved/dob/isapprefallowed?appOrigin=${origin}`);
+
+        if (!response.ok) {
+            callback(null, DENY_CORS_OPTION);
+            return;
+        }
+
+        verifiedOriginsCache.add(origin);
         callback(null, ALLOW_CORS_OPTION);
     } catch (err) {
         callback(err, DENY_CORS_OPTION);
